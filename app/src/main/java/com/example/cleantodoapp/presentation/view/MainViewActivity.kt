6dd64dda62ed5e001bc69205ce647786d38b6597
user <user@mail.com>
2024-04-11 21:30:26 -0500

    added navgraph to mainViewActivity

--- app/src/main/java/com/example/cleantodoapp/presentation/view/MainViewActivity.kt
+++ app/src/main/java/com/example/cleantodoapp/presentation/view/MainViewActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
 import com.example.cleantodoapp.R
 
 class MainViewActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/example/cleantodoapp/presentation/viewmodel/TodoListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cleantodoapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class TodoListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

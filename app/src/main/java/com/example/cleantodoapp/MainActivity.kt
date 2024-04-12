8bc3e993ed8b1a005818a272e9cf10c0f2a62f68
user <user@mail.com>
2024-04-11 21:10:32 -0500

    added login and create account views components
    added views for loginFragment
    
    added jetpack navigation for fragments

--- app/src/main/java/com/example/cleantodoapp/MainActivity.kt
+++ app/src/main/java/com/example/cleantodoapp/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.cleantodoapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cleantodoapp.databinding.ActivityMainBinding
+import com.example.cleantodoapp.presentation.view.MainViewActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initViews()
+
+    }
+
+    private fun initViews() {
+        binding.layoutsButton.setOnClickListener {
+            val navigateIntent = Intent(this, MainViewActivity::class.java)
+            startActivity(navigateIntent)
         }
     }
 }
---
+++ app/src/main/java/com/example/cleantodoapp/presentation/viewmodel/CreateAccountViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cleantodoapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CreateAccountViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
